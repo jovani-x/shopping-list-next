8c8781f7ed9b8c973a30853162571e485e88e22a
jovani-x <jovani-x@users.noreply.github.com>
2024-06-17 11:42:59 +0200

    improve Cards component

--- src/app/components/Cards/Cards.tsx
+++ src/app/components/Cards/Cards.tsx
@@ -3,23 +3,23 @@
 import Card, { ICard } from "@/app/components/Card/Card";
 import CardFilter from "@/app/components/CardFilter/CardFilter";
 import { useCardsFilterContext } from "@/app/components/ProvideCardsFilterContext/ProvideCardsFilterContext";
+import { useTranslation } from "react-i18next";
 
 const Cards = ({ cards }: { cards: ICard[] }) => {
   const { filterState, setFilterState } = useCardsFilterContext();
+  const { t } = useTranslation();
 
-  const renderedCards = !cards?.length ? (
-    <></>
+  const renderedCards = cards.map((card: ICard) => {
+    const isDone =
+      card?.products?.find((product) => !product.got) === undefined;
+    const isShown =
+      (isDone && filterState.done) || (!isDone && filterState.unfinished);
+    return isShown && <Card key={card.id} card={card} />;
+  });
+
+  return !cards?.length ? (
+    <h2 className="mb-2">{t("noCards")}</h2>
   ) : (
-    cards.map((card: ICard) => {
-      const isDone =
-        card?.products?.find((product) => !product.got) === undefined;
-      const isShown =
-        (isDone && filterState.done) || (!isDone && filterState.unfinished);
-      return isShown && <Card key={card.id} card={card} />;
-    })
-  );
-
-  return (
     <>
       <CardFilter filterState={filterState} setFilterState={setFilterState} />
       {renderedCards}

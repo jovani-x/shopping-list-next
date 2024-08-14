0d48588b1ebc170ed7d59e6e11c6aba70593bb20
jovani-x <jovani-x@users.noreply.github.com>
2024-08-14 17:45:52 +0200

    Add env-loader for .env.test

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { loadEnv } from "vite";
 
 export default defineConfig({
   plugins: [tsconfigPaths(), react()],
   test: {
     environment: "jsdom",
     environmentMatchGlobs: [["**/*.node.test.ts", "node"]],
+    env: loadEnv("test", process.cwd(), ""),
     setupFiles: "./src/tests/setup.ts",
     coverage: {
       include: ["src/**", "!src/tests/**", "!src/i18nConfig.js"],

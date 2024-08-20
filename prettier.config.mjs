cf058d6a7ac15ac7ea4869c5875b4fc706ed6a60
jovani-x <jovani-x@users.noreply.github.com>
2024-08-20 18:26:59 +0200

    Change prettier config-file name

---
+++ prettier.config.mjs
@@ -0,0 +1,11 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+	semi: true,
+	singleQuote: false,
+	trailingComma: "es5"
+}
+
+export default config;

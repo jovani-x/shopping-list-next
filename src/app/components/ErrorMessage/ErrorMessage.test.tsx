8fffe21d5c22445ff0698c5cfa189184450c1e0f
jovani-x <jovani-x@users.noreply.github.com>
2024-09-25 13:03:14 +0200

    Improve ErrorMessage component and update test

--- src/app/components/ErrorMessage/ErrorMessage.test.tsx
+++ src/app/components/ErrorMessage/ErrorMessage.test.tsx
@@ -3,13 +3,89 @@
  */
 import { render } from "@testing-library/react";
 import { describe, it, expect } from "vitest";
-import ErrorMessage from "./ErrorMessage";
+import ErrorMessage, { transformErrIntoText, wrapIntoP } from "./ErrorMessage";
+import { ReactElement } from "react";
 
 describe("ErrorMessage", () => {
   const errorMsg = "It's a error message";
 
-  it("Rendered", () => {
-    const { getByText } = render(<ErrorMessage text={errorMsg} />);
-    expect(getByText(errorMsg)).toBeInTheDocument();
+  it("Rendered text", () => {
+    const { getByText, getAllByRole } = render(
+      <ErrorMessage text={errorMsg} />
+    );
+    const res = transformErrIntoText(errorMsg) as ReactElement[];
+    const expArr = [errorMsg];
+
+    expect(getAllByRole("paragraph")).toHaveLength(expArr.length);
+
+    expArr.forEach((expEl, ix) => {
+      expect(getByText(expEl)).toBeInTheDocument();
+      expect(res[ix]?.type).toBe("p");
+      expect(res[ix]?.key).toBe(`${ix}`);
+      expect(res[ix]?.props?.children).toBe(expEl);
+    });
+  });
+
+  it("Rendered object", () => {
+    const obj = { message: errorMsg, reason: "test reason" };
+    const { getByText, getAllByRole } = render(<ErrorMessage text={obj} />);
+    const res = transformErrIntoText(obj) as ReactElement[];
+    const expArr = [`message: ${obj.message}`, `reason: ${obj.reason}`];
+
+    expect(getAllByRole("paragraph")).toHaveLength(expArr.length);
+
+    expArr.forEach((expEl, ix) => {
+      expect(getByText(expEl)).toBeInTheDocument();
+      expect(res[ix]?.type).toBe("p");
+      expect(res[ix]?.key).toBe(`${ix}`);
+      expect(res[ix]?.props?.children).toBe(expEl);
+    });
+  });
+
+  it("Rendered array", () => {
+    const expArr = [errorMsg, "test message 2"];
+    const { getByText, getAllByRole } = render(<ErrorMessage text={expArr} />);
+    const res = transformErrIntoText(expArr) as ReactElement[];
+
+    expect(getAllByRole("paragraph")).toHaveLength(expArr.length);
+    expArr.forEach((expEl, ix) => {
+      expect(getByText(expEl)).toBeInTheDocument();
+      expect(res[ix]?.type).toBe("p");
+      expect(res[ix]?.key).toBe(`${ix}`);
+      expect(res[ix]?.props?.children).toBe(expEl);
+    });
+  });
+
+  it.each([undefined, null])("Rendered %s", (value) => {
+    const { container } = render(<ErrorMessage text={value} />);
+
+    expect(container).toBeEmptyDOMElement();
+  });
+
+  it("wrapIntoP text", () => {
+    const ix = 0;
+    const res = wrapIntoP(errorMsg, ix);
+
+    expect(res?.type).toBe("p");
+    expect(res?.key).toBe(`${ix}`);
+    expect(res?.props?.children).toBe(errorMsg);
+  });
+
+  it("wrapIntoP [key, value]", () => {
+    const ix = 1;
+    const key = "message";
+    const value = errorMsg;
+    const expArr = [key, value];
+    const res = wrapIntoP(expArr, ix);
+
+    expect(res?.type).toBe("p");
+    expect(res?.key).toBe(`${ix}`);
+    expect(res?.props?.children).toBe(`${String(key)}: ${String(value)}`);
+  });
+
+  it.each([true, false, 0, 100, -10])("transformErrIntoText %s", (value) => {
+    const res = transformErrIntoText(value);
+
+    expect(res).toBe(String(value));
   });
 });
--- src/app/components/ErrorMessage/ErrorMessage.tsx
+++ src/app/components/ErrorMessage/ErrorMessage.tsx
@@ -1,13 +1,33 @@
 import errorMessageStyles from "./errorMessage.module.scss";
 
-const ErrorMessage = ({ text }: { text: string }) => {
+const ErrorMessage = ({ text }: { text: unknown }) => {
+  if (text === undefined || text === null) return null;
+
+  const res = transformErrIntoText(text);
+
   return (
     <span role="alert" className={errorMessageStyles.error}>
-      {text.split("\n").map((el, ix) => {
-        return <p key={ix}>{el}</p>;
-      })}
+      {res}
     </span>
   );
 };
+
+export const wrapIntoP = (el: unknown, ix: number) => {
+  if (Array.isArray(el)) {
+    const [key, value] = el;
+    return <p key={ix}>{`${String(key)}: ${String(value)}`}</p>;
+  } else {
+    return <p key={ix}>{String(el).replaceAll('":"', ": ")}</p>;
+  }
+};
+
+export const transformErrIntoText = (text: unknown) =>
+  typeof text === "string"
+    ? text.split("\n").map(wrapIntoP)
+    : Array.isArray(text)
+      ? text.map(wrapIntoP)
+      : typeof text === "object"
+        ? JSON.stringify(text).slice(2, -2).split('","').map(wrapIntoP)
+        : String(text);
 
 export default ErrorMessage;
